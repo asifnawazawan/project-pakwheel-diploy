f023b5f393bdda840dea0b4ad82d439645de3e26
Asif Nawaz <acifnawaz@gmail.com>
2024-11-02 00:45:52 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ export default function Home(){
   </div>
   <Herosec />
   <Featuring />
-  {/* <Details /> */}
   <Footer />
 </div>
 
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function footer(){
     return(
 <div>
-    <footer className="bg-white py-3 px-3 text-justify font-medium text-black">@Project By Asif Nawaz</footer>
+    <footer className="bg-white 500 py-1 px-1 text-justify font-medium text-black">@Project By Asif Nawaz</footer>
 </div>
 
 
