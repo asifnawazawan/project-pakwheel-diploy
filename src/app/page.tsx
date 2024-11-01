27a822dcc72892e0cdc01e3c411b29b3f45265bf
Asif Nawaz <acifnawaz@gmail.com>
2024-11-01 23:09:08 +0500

    its my project

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+
+import Navbar from "@/components/navbar"
+import Herosec from "@/components/herosec"
+import Featuring from "@/components/featuring";
+
+import Footer from "@/components/footer";
+
+export default function Home(){
+  return (
+<div>
+  < Navbar />
+  <div className=" h-20">
+
+  </div>
+  <Herosec />
+  <Featuring />
+  {/* <Details /> */}
+  <Footer />
+</div>
+
+  );
+}
+
+    
+ 
+  
---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function footer(){
+    return(
+<div>
+    <footer className="bg-white py-3 px-3 text-justify font-medium text-black">@Project By Asif Nawaz</footer>
+</div>
+
+
+    )
+}
